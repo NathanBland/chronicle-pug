e1009448460a186e801fd2c7e9d5e6b8529f8da1
Nathan Bland <nathan.bland@gmail.com>
2016-04-20 11:13:06 -0600

    Switching from base repo to own

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var routes = require('./routes/')
+var sass = require('node-sass-middleware')
+var app = express()
+
+app.set('port', 8080)
+app.set('ip', '0.0.0.0')
+
+app.set('view engine', 'pug')
+app.use(
+  sass({
+    root: __dirname,
+    indentedSyntax: true,
+    src: '/sass',
+    dest: '/public/css',
+    prefix: '/css',
+    debug: true
+  })
+)
+
+app.use(express.static('public'))
+
+app.use(routes(app, express))
+
+var server = app.listen(app.get('port'), app.get('ip'), function () {
+  console.log('Chronicle UI has started...')
+})
